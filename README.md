2b32cc12a881bfd013086311e2ce46a4c8313c81
Yassine el Ouazzani <yassine.elouazzani@jumbo.com>
2024-03-05 17:02:12 +0100

    step -01

---
+++ README.md
@@ -0,0 +1,8 @@
+# In this chapter
+
+- import non JS files
+- Showcase what happens when you export different types (example object vs string)
+- Build for prod
+- Pass params to configure your plugin
+  - use case replace `splitter`
+- apply: `serve` | `build`
--- plugins/arrow-parser.js
+++ plugins/arrow-parser.js
@@ -1,22 +1,11 @@
-import { createSVG } from "../utils/create-svg";
-export default ({ splitter = '->' }) => ({
+export default ({ splitter = '->' } = {}) => ({
   name: 'arrow-parser',
   async transform(src, id) {
     if (/\.arrow$/.test(id)) {
       const data = src.split(splitter).map(item => (item || '').trim());
-      const svg = createSVG(data);
       return {
-        code: `export default '${svg}'`,
+        code: `export default ${JSON.stringify(data)}`,
       }
     }
   },
-  transformIndexHtml: {
-    handler(html, ctx) {
-      // console.log('TRANFORM', html)
-      return {
-        html: html.replace('Pipeline', '<pre>Hi</pre>'),
-        tags: [],
-      }
-    }
-  }
 });
